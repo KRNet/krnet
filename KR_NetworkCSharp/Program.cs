090cc5e774e8cb278005c5774c0868abc659f999
MaxMyalkin <maxmyalkin@gamil.com>
2014-03-06 10:13:48 +0400

    added forms

---
+++ KR_NetworkCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace KR_NetworkCSharp
+{
+    class Program
+    { /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainMenu());
+        }
+    }
+
+
+}
