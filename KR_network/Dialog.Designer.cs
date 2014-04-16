1cfc5d5713fc72d9f9560edee20a735fdc3a3ed8
MaxMyalkin <maxmyalkin@gamil.com>
2014-04-16 12:27:21 +0400

    dialog changes

--- KR_network/Dialog.Designer.cs
+++ KR_network/Dialog.Designer.cs
@@ -39,7 +39,7 @@
             // richTextBox1
             // 
             this.richTextBox1.Location = new System.Drawing.Point(12, 208);
-            this.richTextBox1.MaxLength = 255;
+            this.richTextBox1.MaxLength = 200;
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(309, 68);
             this.richTextBox1.TabIndex = 0;
